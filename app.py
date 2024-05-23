ec28cea4c2d51af45af74436c207c464331dec74
phoenixHac <emilfsdk@gmail.com>
2024-05-23 10:19:06 +0200

    feat: Add chat functionality with SocketIO

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 from flask import Flask, redirect, request, render_template
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user  # Add current_user here
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_socketio import SocketIO, emit
 from auth import authenticate, User
 
-
-
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_is_not_thisone'  # Change this to a random secret key
+app.secret_key = 'your_secret_key_is_not_thisone'
 login_manager = LoginManager()
 login_manager.init_app(app)
+socketio = SocketIO(app)
 
-server_uri = "ldap://10.0.0.4:389"  # No TLS, standard LDAP port
+server_uri = "ldap://10.0.0.4:389"
 domain = "A10.dk"
 
 @login_manager.user_loader
 def load_user(username):
-    # This callback is used to reload the user object from the user ID stored in the session
     return User(username)
 
 @app.route("/", methods=['POST', 'GET'])
@@ -23,12 +22,11 @@ def login():
         username = request.form['username']
         password = request.form['password']
         try:
-            global user # TEST
+            global user
             user = authenticate(server_uri, domain, username, password)
             login_user(user)
             username = user.username
             groups_user = user.groups
-            # return redirect("/loggedin")
             return render_template("loggedin.html", user=username, groups=groups_user)
         except ValueError as err:
             return render_template("login.html", error=str(err))
@@ -53,10 +51,17 @@ def logout():
 def show():
     groups_user = current_user.groups
     logged_user = current_user.username
-    
     return render_template("show.html", groups=groups_user, user=logged_user)
 
+@app.route("/it-chat", methods=["GET","POST"])
+@login_required
+def itchat():
+    return render_template("it-chat.html", user=current_user.username)
 
+@socketio.on('send_message')
+def handle_send_message_event(data):
+    app.logger.info("{} has sent message to the room: {}".format(data['username'], data['message']))
+    emit('receive_message', data, broadcast=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
