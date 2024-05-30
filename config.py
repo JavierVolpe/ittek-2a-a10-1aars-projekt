49fbeebf6d9f237e70483bfb33fb0c17e2933368
Javier Volpe <143789444+JavierVolpe@users.noreply.github.com>
2024-05-30 11:34:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 class Config:
     DATABASE_URI = "sqlite:///app.db"
     PAGE_SIZE = 5
-    SECRET_KEY = "your_secret_key_is_not_thisone"
+    SECRET_KEY = "your_secret_key"
     DEBUG = True
     NEWS_DATABASE = 'database.db'
 
     LDAP_SERVER_URI = "ldap://10.0.0.4:389"
     LDAP_DOMAIN = "A10.dk"
     LDAP_SERVICE_USERNAME = "A10"
-    LDAP_SERVICE_PASSWORD = "Password123!"
+    LDAP_SERVICE_PASSWORD = ""
 
     SERVER_PORT = 443 
 
